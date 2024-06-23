a995a5d7f0984fad6c4ba5fc9665860367941cc1
Алексей <lobashev.aleks.88@gmail.com>
2024-06-23 13:55:35 +0600

    Seminar_02 added

---
+++ Seminar_02/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает
+// на вход число и проверяет, кратно ли 
+// оно одновременно 7 и 23.
+
+System.Console.Write("Введите число: ");
+
+int result = Convert.ToInt32(Console.ReadLine());
+{
+    if (result % 7 == 0 && result % 23 == 0)
+    {
+        System.Console.WriteLine("Число кратное");
+    }
+    else
+    {
+        System.Console.WriteLine("Число не кратное");
+    }
+}
---
+++ Seminar_02/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход координаты
+// точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной
+// четверти плоскости, в которой находится эта точка.
+
+System.Console.Write("ВВедите значение X: ");
+int X = Convert.ToInt32 (Console.ReadLine());
+
+System.Console.Write("Введите значение Y: ");
+int Y = Convert.ToInt32 (Console.ReadLine());
+
+if (X > 0 && Y >0)
+{
+    System.Console.WriteLine("1");
+}
+else if (X < 0 && Y > 0)
+{
+    System.Console.WriteLine("2");
+}
+else if (X < 0 && Y < 0)
+{
+    System.Console.WriteLine("3");
+}
+else if (X > 0 && Y < 0)
+{
+    System.Console.WriteLine("4");
+}
+else 
+{
+    System.Console.WriteLine("Значения не должны быть равны 0");
+}
