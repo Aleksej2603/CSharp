a4ab8ee71add4fb7bc9e37fc980f2a9906287bf9
Алексей <lobashev.aleks.88@gmail.com>
2024-06-23 15:17:08 +0600

    Добавлены задания к Семинару 6

---
+++ Seminar_06/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте массив символов (тип char []). Создайте строку из символов этого массива.
+// Указание
+// Конструктор строки вида string(char []) не использовать.
+// Пример
+// [‘a’, ‘b’, ‘c’, ‘d’] => “abcd”
+
+char[] letters = { 'a', 'b', 'c', 'd' };
+
+// 1-й способ:
+// string str = new string(chars);
+
+// 2-й способ:
+// string str = "";
+// foreach (char letter in letters)
+// str += letter;
+
+// 3-й способ:
+// string str = string.Join("", letters);
+
+// 4-й способ:
+string str = string.Concat(letters);
+
+System.Console.WriteLine(str);
---
+++ Seminar_06/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// На основе символов строки (тип string) сформировать массив символов (тип char[]).
+// Вывести массив на экран.
+// Указание
+// Метод строки ToCharArray() не использовать.
+// Пример
+// “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ]
+
+void Main()
+{
+System.Console.Write("Введите строку: ");
+string? str = Console.ReadLine();
+char[] chars = StringToCharArray(str);
+PrintArray(chars);
+}
+
+void PrintArray(char[] chars)
+{
+Console.WriteLine("[" + string.Join(", ", chars) + "]");
+}
+
+char[] StringToCharArray(string? inputString)
+{
+char[] chars = new char[inputString!.Length];
+
+for (int i = 0; i < chars.Length; i++)
+chars[i] = inputString[i];
+
+return chars;
+}
+
+Main();
