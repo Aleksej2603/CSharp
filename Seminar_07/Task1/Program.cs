688eb9e08f6df941f145f7a43f5c44538149298b
Алексей <lobashev.aleks.88@gmail.com>
2024-06-27 09:08:05 +0600

    Добавлены домашние задания к семинару 7

---
+++ Seminar_07/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿//Нпишите программу, которая будет
+//принимать на вход число и 
+//возвращать сумму его цифр
+
+//Указание
+//Использовать рекурсию
+
+//Пример
+//123 => 6
+//63 => 9
+
+void Main()
+{
+
+    int number = ReadInt("Введите число: ");
+    SumNumbers(number);
+    System.Console.WriteLine(SumNumbers(number));
+
+}
+
+int SumNumbers(int number)
+{
+    if (number == 0) return 0;
+    return number % 10 + SumNumbers(number / 10);
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+Main();
